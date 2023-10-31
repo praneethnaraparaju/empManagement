fbd6ddae07b2586cd01e0de4344d2f10ae63132f
praneethnaraparaju <63139009+praneethnaraparaju@users.noreply.github.com>
2023-10-31 16:25:02 +0530

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -13,9 +13,18 @@ func SetupRoutes(router *gin.Engine) {
         api.GET("/employees/:user_id", controllers.GetEmployeeDetails)
         api.DELETE("/employees/:user_id", controllers.DeleteEmployee)
 
-		api.POST("/departments", controllers.CreateDepartment)
+	api.POST("/departments", controllers.CreateDepartment)
         api.GET("/departments", controllers.GetDepartments)
         api.GET("/departments/:id", controllers.GetDepartmentDetails)
         api.DELETE("/departments/:id", controllers.DeleteDepartment)
+
+	api.POST("/compliance", CreateCompliance)
+        api.GET("/compliance", GetComplianceDocuments)
+        api.GET("/compliance/:userld", GetComplianceByUser)
+
+        api.POST("/statusreport", CreateStatusReport)
+        api.GET("/statusreport/:userld/:compid", GetStatusReports)
+
+        api.POST("/user/validate", ValidateUser)
     }
 }
